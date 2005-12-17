1d49c750d128e176652538d5e4aa4d27fbc8e39e
tg <tg@mirbsd.org>
2005-12-17 05:46:08 +0000

    big fat licence update (I left some which are bsiegert@'s alone though) also, remove licence boilerplate from some .h files who don't deserve it and remove and add some advertising clauses because I say so

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/mksh
-# $MirOS: contrib/code/jupp/autogen.sh,v 1.6 2005/08/26 23:41:14 tg Exp $
+# $MirOS: contrib/code/jupp/autogen.sh,v 1.7 2005/12/17 05:46:08 tg Exp $
 #-
 # Copyright (c) 2004, 2005
 #	Thorsten "mirabile" Glaser <tg@66h.42h.de>
@@ -15,13 +15,14 @@
 # ware must display the following acknowledgement:
 #	This product includes material provided by Thorsten Glaser.
 #
-# Licensor hereby provides this work "AS IS" and WITHOUT WARRANTY of
-# any kind, expressed or implied, to the maximum extent permitted by
-# applicable law, but with the warranty of being written without ma-
-# licious intent or gross negligence; in no event shall licensor, an
-# author or contributor be held liable for any damage, direct, indi-
-# rect or other, however caused, arising in any way out of the usage
-# of this work, even if advised of the possibility of such damage.
+# Licensor offers the work "AS IS" and WITHOUT WARRANTY of any kind,
+# express, or implied, to the maximum extent permitted by applicable
+# law, without malicious intent or gross negligence; in no event may
+# licensor, an author or contributor be held liable for any indirect
+# or other damage, or direct damage except proven a consequence of a
+# direct error of said person and intended use of this work, loss or
+# other issues arising in any way out of its use, even if advised of
+# the possibility of such damage or existence of a nontrivial bug.
 
 if [[ -z $AUTOCONF_VERSION ]]; then
 	AUTOCONF_VERSION=2.59
